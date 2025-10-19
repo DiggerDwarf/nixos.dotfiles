6e33627f520b31bf891059d7c0222a00c79a065a
DiggerDwarf <135308092+DiggerDwarf@users.noreply.github.com>
2025-10-19 22:53:56 +0200

    hyprland2

--- modules/hypr/hyprland.nix
+++ modules/hypr/hyprland.nix
@@ -6,18 +6,20 @@ in {
     home.file.".config/hypr/hyprland".source = ./../../files/hyprland.conf;
     wayland.windowManager.hyprland = {
         enable = false;
-        monitor = [
-            "DP-1, preferred, 0x0, auto"
-            "DP-2, preferred, 0x0, auto"
-            "HDMI-A-1, preferred, 0x0, auto"
-            "eDP-1, 2880x1800@120, auto, 1.5"
-        ];
-        exec-once = [
-            "swww-daemon"
-            "quickshell"
-        ];
-        env = [
-            "XCURSOR"
-        ];
+        settings = {
+            monitor = [
+                "DP-1, preferred, 0x0, auto"
+                "DP-2, preferred, 0x0, auto"
+                "HDMI-A-1, preferred, 0x0, auto"
+                "eDP-1, 2880x1800@120, auto, 1.5"
+            ];
+            exec-once = [
+                "swww-daemon"
+                "quickshell"
+            ];
+            # env = [
+            #     "XCURSOR"
+            # ];
+        };
     };
 }
